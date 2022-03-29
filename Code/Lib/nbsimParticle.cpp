8e66df75ab81e3e38d3f00cbe8aa01046e447625
Xenofon-Chiotopoulos <92038142+Xenofon-Chiotopoulos@users.noreply.github.com>
2022-03-29 17:15:20 +0000

    Creating getters for private member vairables

--- Code/Lib/nbsimParticle.cpp
+++ Code/Lib/nbsimParticle.cpp
@@ -3,15 +3,15 @@
 
 Particle::Particle()
 {
-    Eigen::Vector3d origin(0,0,0);
-    position_ = origin;
-    velocity_ = origin;
+  Eigen::Vector3d origin(0,0,0);
+  position_ = origin;
+  velocity_ = origin;
 }
 
 Particle::Particle(Eigen::Vector3d &position, Eigen::Vector3d &velocity)
 {
-    position_ = position;
-    velocity_ = velocity;
+  position_ = position;
+  velocity_ = velocity;
 }
 
 Particle::~Particle()
@@ -19,20 +19,30 @@ Particle::~Particle()
 
 }
 
+
+void Particle::setPosition(Eigen::Vector3d position)
+{
+  position_ = position;
+}
+
 Eigen::Vector3d Particle::getPosition()
 {
-    return position_;
+  return position_;
+}
+
+void Particle::setVelocity(Eigen::Vector3d velocity)
+{
+  velocity_ = velocity;
 }
 
 Eigen::Vector3d Particle::getVelocity()
 {
-    return velocity_;
+  return velocity_;
 }
 
 void Particle::integrateTimestep(Eigen::Vector3d &acceleration, double timestep)
 {
-    velocity_ = velocity_ + acceleration * timestep;
-    //velocity_ = velocity_ - position_ * timestep;
-    position_ = position_ + velocity_ * timestep;
-    
+  
+  position_ = position_ + velocity_ * timestep;
+  velocity_ = velocity_ + acceleration * timestep;
 }
--- Code/Lib/nbsimParticle.h
+++ Code/Lib/nbsimParticle.h
@@ -2,15 +2,17 @@
 
 class Particle{
 
-    public:
-    Particle();
-    Particle(Eigen::Vector3d &position, Eigen::Vector3d &velocity);
-    ~Particle();
-    Eigen::Vector3d getPosition();
-    Eigen::Vector3d getVelocity();
-    void integrateTimestep(Eigen::Vector3d &acceleration, double timestep);
-
-    private:
-    Eigen::Vector3d position_;
-    Eigen::Vector3d velocity_;
+  public:
+  Particle();
+  Particle(Eigen::Vector3d &position, Eigen::Vector3d &velocity);
+  ~Particle();
+  void setPosition(Eigen::Vector3d position);
+  Eigen::Vector3d getPosition();
+  void setVelocity(Eigen::Vector3d velocity);
+  Eigen::Vector3d getVelocity();
+  void integrateTimestep(Eigen::Vector3d &acceleration, double timestep);
+  
+  private:
+  Eigen::Vector3d position_;
+  Eigen::Vector3d velocity_;
 };
